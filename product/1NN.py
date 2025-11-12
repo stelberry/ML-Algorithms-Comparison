55262915a01d1b6b104cf6cb66ea1b512fdbe18f
Yoon Phyo Ei <wmis161@live.rhul.ac.uk>
2025-11-12 02:43:12 +0000

    #1 added detailed comments

--- product/1NN.py
+++ product/1NN.py
@@ -16,16 +16,25 @@ print("Testing data shape X_test:",X_test.shape)
 
 #1NN Function
 def predict_1nn (dataset, label, test_point):
-#calculate the euclidean distance 
-  euclidean_distance = np.linalg.norm(dataset - test_point, axis = 1) #Compute norm across each row
+  """
+  Compute the Euclidean distance between the test_point and each row in the dataset
+  For example, datasets are [1,2] and [3,1] and test_point is [4,2].
+  
+  1st: (dataset- testpoint),
+  Row 0: [1,2] - [4,2] = [-3,0]
+  Row 1: [3,1] - [4,2] = [-1,-1]
+  
+  2nd: np.linalg.norm numpy function compute the norm length(squared each then sum all then sqrt that sum)
+  """
+  euclidean_distance = np.linalg.norm(dataset - test_point, axis = 1)
   target = np.argmin(euclidean_distance) #return the index of the smallest value
   return label[target]
 
-"""dataset = np.array([[1,2],[3,1],[1,1]])
+dataset = np.array([[1,2],[3,1],[1,1]])
 label = ['1', '2', '1']
 test_point = np.array([4,2])
 predict = predict_1nn(dataset, label, test_point)
-print("predicted label: ", predict)"""
+print("predicted label: ", predict)
 
 #empty list to store the predictions
 evaluation_arr = []
--- product/kNN.py
+++ product/kNN.py
@@ -2,7 +2,16 @@ import numpy as np
 from collections import Counter
 
 def predict_kNN(dataset, labels, test_point, k):
-  # calculate the distances
+  """
+  Compute the Euclidean distance between the test_point and each row in the dataset
+  For example, datasets are [1,2] and [3,1] and test_point is [4,2].
+  
+  1st: (dataset- testpoint),
+  Row 0: [1,2] - [4,2] = [-3,0]
+  Row 1: [3,1] - [4,2] = [-1,-1]
+  
+  2nd: np.linalg.norm numpy function compute the norm length(squared each then sum all then sqrt that sum)
+  """
   euclidean_distance = np.linalg.norm(dataset - test_point, axis = 1)
   print("distances:", euclidean_distance)
   
