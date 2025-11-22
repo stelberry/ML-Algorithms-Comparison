2914efea37d90978f8bedfcf777e59fc0f1f5b5e
Yoon Phyo Ei <wmis161@live.rhul.ac.uk>
2025-11-22 23:59:42 +0000

    #6 add weighted_gini logic

--- product/decision_trees.py
+++ product/decision_trees.py
@@ -12,13 +12,7 @@ class Node:
   def is_leaf(self):
     return self.value is not None
     
- 
-  
-
-
-
-
-
+class DecisionTreesCart:
 
   def calc_entropy(labels):
     class_names, class_counts = np.unique(labels, return_count = True)
@@ -26,11 +20,25 @@ class Node:
     entropy = -np.sum(probs * np.log2(probs))
     return entropy
   
-  def calc_gini_impurity(labels):
+  def calc_gini_impurity(self, labels):
     class_names, class_counts = np.unique(labels, return_count = True)
-    probs = class_counts/class_counts.sum
-    gini_impurity = 1.0 - np.sum(probs ** 2)
+    probabilities = class_counts/class_counts.sum
+    gini_impurity = 1.0 - np.sum(probabilities ** 2)
     return gini_impurity
 
+  def clac_weighted_gini(self, left_labels, right_labels):
+    total = left_labels.sum + right_labels.sum
+    
+    gini_left = self.calc_gini_impurity(left_labels)
+    gini_right = self.calc_gini_impurity(right_labels)
+    
+    left_weight = left_labels.sum/total
+    right_weight = right_labels.sum/total
+    
+    weighted_gini = (left_weight * gini_left) + (right_weight * gini_right)
+  
+    return weighted_gini
+    
+  
 
 
