df743ccfb5c9574bc98308679c7e91a7773e6565
Yoon Phyo Ei <wmis161@live.rhul.ac.uk>
2025-11-27 00:40:03 +0000

    #4 fixed error, predicted lables, calculated error rate

--- product/credit_card_1nn.py
+++ product/credit_card_1nn.py
@@ -3,12 +3,18 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.utils import resample
 from one_NN import predict_1nn
+import os
 
 def credit_card_1nn():
+  filename = 'product/default_of_credit_card_clients.xls'
+    
+  print(f"Loading '{filename}'... (this might take a few seconds)")
+    
   try:
-    df = pd.read_csv('defalut of credit card clients.csv')
+    df = pd.read_excel(filename, header=1)
   except FileNotFoundError:
-    print("csv file not found")
+    print(f"\nERROR: Could not find '{filename}'.")
+    print(f"Current folder: {os.getcwd()}")
     return
   
   print("Dataset shape:", df.shape)
@@ -26,7 +32,7 @@ def credit_card_1nn():
     df = df.drop('ID', axis = 1)
   
   X = df.drop(target_name, axis =1).values
-  y = df[target_name].value
+  y = df[target_name].values
   
   feature_names = df.drop(target_name, axis=1).columns
   
@@ -34,7 +40,7 @@ def credit_card_1nn():
   
   # since 30,000 rows is too slow for a simple 1NN loop, I sample 1,000 rows for testing.
   # stratify=y to keep the same % of default/non-default.
-  X, y = resample(X, y, n_samples=1000, random_state=42, stratify=y)
+  X, y = resample(X, y, n_samples=1000, random_state=2802, stratify=y)
   print("Speeded Sampled Shape:", X.shape)
   
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0, stratify=y)
@@ -43,4 +49,26 @@ def credit_card_1nn():
   print(f"Testing data shape: {X_test.shape}")
     
   print("\nTraining 1NN model...")
-  print("This may take a while for large datasets...")
+  
+  evaluation_arr = []
+  print("Starting predictions (this might take a moment)...")
+  
+  for test_point in X_test:
+    predict = predict_1nn(X_train, y_train, test_point)
+    evaluation_arr.append(predict)
+    
+  y_pred = np.array(evaluation_arr)
+  
+  accuracy_score = np.mean(y_pred == y_test)
+  error_rate = 1 - accuracy_score
+  
+  print("Predicted labels (first 10): ", y_pred[:10])
+  print("Actual labels (first 10):    ", y_test[:10])
+  print("Accuracy score:", accuracy_score)
+  print("Error rate:",error_rate)
+  
+  
+  
+if __name__ == "__main__":
+  credit_card_1nn()
+  
